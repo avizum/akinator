6954c72e1e969d0ccd2f2cdcbcaa57f7748b1bb2
avizum <juliusrt@outlook.com>
2022-09-11 13:28:39 -0700

    [1.5.2] Version Bump

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ from typing import Any
 project: str = "asyncakinator"
 copyright: str = "2022, avizum"
 author: str = "avizum"
-release: str = "1.5.1"
+release: str = "1.5.2"
 
 
 # -- General Configuration --
